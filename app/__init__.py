14dd2fe53d3cac942699017894131c5f760161a3
Jeyfel Brandauer M <jeyfelbrandauer@gmail.com>
2017-02-05 17:23:48 +0000

    Use flask blueprint to organize routes/views

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,32 @@
 import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 from settings import config
 
-app = Flask(__name__)
-app.config.from_object(config[os.getenv('FLASK_CONFIG') or 'default'])
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-if not app.debug:
-    import logging
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config[os.getenv('FLASK_CONFIG') or 'default'])
+    db.init_app(app)
+    
+    if not app.debug:
+        import logging
 
-    fmt = "%(levelname)s - %(asctime)s %(filename)s:%(lineno)d %(message)s"
-    formatter = logging.Formatter(fmt=fmt)
-    log_path = '/var/log/flask/{}.log'.format(app.name)
-    file_handler = logging.FileHandler(log_path)
-    file_handler.setFormatter(formatter)
+        fmt = "%(levelname)s - %(asctime)s %(filename)s:%(lineno)d %(message)s"
+        formatter = logging.Formatter(fmt=fmt)
+        log_path = '/var/log/flask/{}.log'.format(app.name)
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(formatter)
 
-    app.logger.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+
+    # Register app blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    return app
